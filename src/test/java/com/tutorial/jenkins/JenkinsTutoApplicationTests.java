dd226990897d74a37e10943c4106ec28022c08c2
Fethi <fe.benseddik@gmail.com>
2021-10-30 11:42:22 +0200

    modification properties

--- src/test/java/com/tutorial/jenkins/JenkinsTutoApplicationTests.java
+++ src/test/java/com/tutorial/jenkins/JenkinsTutoApplicationTests.java
@@ -6,8 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
